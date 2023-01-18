8953dd2be90a128b4df2b607d3db6c6fe5afa3bf
samathi <samathisapumana@gmail.com>
2023-01-18 10:00:10 -0800

    FizzBuzz porblem using if-else and for loop

--- myFirstConsoleApp/Program.cs
+++ myFirstConsoleApp/Program.cs
@@ -71,6 +71,24 @@ namespace myFirstConsoleApp
              * end of Calculator
              * 
              */
-        }
+            // FizzBuzz problem
+            Console.WriteLine("----------FizzBuzz porblem--------------");
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i%5 == 0 && i%3 ==0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if(i%5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else { Console.WriteLine(i); }
+            }
+      }
     }
 }
