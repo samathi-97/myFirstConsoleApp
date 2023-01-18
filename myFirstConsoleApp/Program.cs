e7b1a60c005c627e2f355cb2c1730de98154a3f6
samathi <samathisapumana@gmail.com>
2023-01-18 08:09:40 -0800

    playing with variables

--- myFirstConsoleApp/Program.cs
+++ myFirstConsoleApp/Program.cs
@@ -11,7 +11,14 @@ namespace myFirstConsoleApp
         static void Main(string[] args)
         {
             Console.WriteLine("hello World...!");
-           // Console.ReadKey(); this make sure you program Stop for a while
+            // Console.ReadKey(); this make sure you program Stop for a while till you observe the result.
+           
+            //playing with variables
+            int firstNumber = 10;
+            int secondNumber = 12;
+
+            int totalAmount = firstNumber + secondNumber;
+            Console.WriteLine(totalAmount);
         }
     }
 }
